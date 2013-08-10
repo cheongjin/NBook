cc9088a0cb967eb4a6bc6ed71ac4e043099c9b95
jin <puding1507@naver.com>
2013-08-10 20:12:49 +0900

    CommonData Make

---
+++ NBook/NBook/CommonNBookViewController.h
@@ -0,0 +1,19 @@
+//
+//  CommonNBookViewController.h
+//  NBook
+//
+//  Created by jin on 13. 8. 10..
+//  Copyright (c) 2013년 jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommonNBookViewController : UIViewController
+
+
+
+- (void)loadData;
+
+
+
+@end
---
+++ NBook/NBook/CommonNBookViewController.m
@@ -0,0 +1,34 @@
+//
+//  CommonNBookViewController.m
+//  NBook
+//
+//  Created by jin on 13. 8. 10..
+//  Copyright (c) 2013년 jin. All rights reserved.
+//
+
+#import "CommonNBookViewController.h"
+
+@interface CommonNBookViewController ()
+
+@end
+
+@implementation CommonNBookViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)loadData
+{
+    
+}
+
+@end
